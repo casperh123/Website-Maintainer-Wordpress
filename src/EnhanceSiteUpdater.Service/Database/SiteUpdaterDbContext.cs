d9bf238590a249993bf702dbb6efac119a2c233c
casho <casho@itu.dk>
2024-10-07 15:31:30 +0200

    Extend IdentityDbContext

--- src/EnhanceSiteUpdater.Service/Database/SiteUpdaterDbContext.cs
+++ src/EnhanceSiteUpdater.Service/Database/SiteUpdaterDbContext.cs
@@ -1,12 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EnhanceSiteUpdater.Service.Database;
 
-public class SiteUpdaterDbContext : DbContext
+public class SiteUpdaterDbContext : IdentityDbContext<IdentityUser>
 {
     public string DbPath { get; }
 
-    public SiteUpdaterDbContext()
+    public SiteUpdaterDbContext(DbContextOptions<SiteUpdaterDbContext> options) : base(options)
     {
         Environment.SpecialFolder folder = Environment.SpecialFolder.LocalApplicationData;
         string path = Environment.GetFolderPath(folder);
