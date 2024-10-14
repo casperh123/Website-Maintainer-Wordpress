00128dcd47ac1e54c20afda24d9f6e586c345fa9
casperh123 <casho@itu.dk>
2024-10-14 10:19:26 +0200

    Add conversion from Enhance Site to core

---
+++ src/EnhanceSiteUpdater.Infrastructure/EntityHelpers/EnhanceToCore.cs
@@ -0,0 +1,27 @@
+using Enhance.Client.Models;
+using EnhanceSiteUpdater.Core.Entities;
+
+namespace EnhanceSiteUpdater.Infrastructure.EntityHelpers;
+
+public class EnhanceToCore
+{
+    public static UpdaterWebsite EnhanceWebsite(Enhance.Client.Models.Website website)
+    {
+        return new UpdaterWebsite(
+            website.Domain.Domain,
+            EnhanceKind(website.Kind ?? WebsiteKind.Normal),
+            MaintenanceType.None
+        );
+    }
+    
+    public static Kind EnhanceKind(WebsiteKind kind) => kind switch
+    {
+        WebsiteKind.Normal => Kind.Normal,
+        WebsiteKind.Roundcube => Kind.Roundcube,
+        WebsiteKind.Staging => Kind.Staging,
+        WebsiteKind.ControlPanel => Kind.ControlPanel,
+        WebsiteKind.PhpMyAdmin => Kind.PhpMyAdmin,
+        WebsiteKind.ServerHostname => Kind.ServerHostname,
+        _ => Kind.Normal
+    };
+}
